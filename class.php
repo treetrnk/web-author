02779b8224812a3b1c8f9d06bb9eaf0427362e46
Nathan Hare <nhare330@gmail.com>
2019-04-26 19:39:33 -0400

    Break children into individual items

--- class.php
+++ class.php
@@ -113,7 +113,7 @@ class Page {
 	public function total_word_count() {
 		$wordcount = 0;
 		$children = $this->children;
-		foreach($children as $child) {	
+		foreach($children->fetch_assoc() as $child) {	
 			$c = new Page($child["id"]);
 			$wordcount += $c->words;
 		}
